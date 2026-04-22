ac4d2248a6ed232fd0301e61216abdabcbff4c00
anchu1024 <anchuyu@outlook.jp>
2026-04-22 22:00:21 +0900

    screen.jsに管理に必要な基本的機能を追加

--- src/js/screen.js
+++ src/js/screen.js
@@ -3,14 +3,43 @@
  * スクリーンのエンティティの描画マネジメントを行う
  */
 class Screen {
+
   /**
-   *
+   * 最大で何人のキャラを出現させるか。
+   * @type {number}
    */
+  #maxChara = 2;
+
   constructor() {
-    this.charaPos = new Array(2);
+    this.charaPos = new Array(this.#maxChara).fill(false);
   }
 
+  /**
+   * キャラクター登場時に衝突が起きないよう場所を確保します。
+   * @return {Object} 確保した場所などを返します。
+   */
   allocate() {
-    let idx = -1;
+    let idx = 0;
+    while(idx < this.charaPos.size()){
+      if(!this.charaPos[idx]){
+        break;
+      }
+      idx++;
+    }
+    if(idx === this.charaPos.size()){
+      throw new RangeError(`想定を超えた人数のキャラを呼び出そうとしました。仕様修正を待つか、人数制限(${this.#maxChara}人)内に収まるようにプログラムを組みなおしてください。`);
+    }
+    this.charaPos[idx] = true;
+    return {
+      idx
+    };
+  }
+
+  /**
+   * 指定した位置のキャラを解放します。これを行うことで次のキャラがその位置に入ることが来ます。
+   * @param {number} idx  解放したいキャラの位置を指定します。
+   */
+  free(idx) {
+    this.charaPos[idx] = false;
   }
 }
